22c35e806642abc3b777197fd4dc4f0fcaac309d
McKitsune <kitsuneidea@gmail.com>
2024-11-05 21:12:54 -0500

    1

--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -5,11 +5,9 @@ import axios from 'axios';
 export class CountryService {
   async getCountryInfo(name: string) {
     try {
-      // Realiza la solicitud a la API pública de países
       const response = await axios.get(`https://restcountries.com/v3.1/name/${name}`);
       const countryData = response.data[0];
 
-      // Extrae los datos necesarios (capital, población, región)
       return {
         name: countryData.name.common,
         capital: countryData.capital[0] || 'No disponible',
